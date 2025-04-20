b85757878a1b93e5ae2bd4675d28ff7bd4f59053
Dani Sanguino <sanguinad@gmail.com>
2025-04-20 14:56:10 +0200

    oculto footer. toco estilos y meto correo en recepción vehículo

--- src/app/pages/nueva-reparacion/nueva-reparacion.component.ts
+++ src/app/pages/nueva-reparacion/nueva-reparacion.component.ts
@@ -7,6 +7,10 @@ import type { Usuario } from '../../interfaces/usuario';
 import { UsuariosService } from '../../services/usuarios.service';
 import { VehiculosService } from '../../services/vehiculos.service';
 import type { Vehiculo } from '../../interfaces/vehiculo';
+import { MailingService } from '../../services/mailing.service';
+import { ClientesService } from '../../services/clientes.service';
+import type { Cliente } from '../../interfaces/cliente';
+import type { Reparacion } from '../../interfaces/reparacion';
 
 @Component({
   selector: 'app-nueva-reparacion',
@@ -21,9 +25,15 @@ export class NuevaReparacionComponent {
   router = inject(Router)
   mecanicos: Usuario[] = []
   vehiculos: Vehiculo[] = []
+
   route = inject(ActivatedRoute)
   clienteId!: number
   activatedRoute = inject(ActivatedRoute);
+  mailingService = inject(MailingService)
+  clientesService = inject(ClientesService)
+  cliente!: Cliente
+  nuevaReparacion!: Reparacion
+  fecha = ''
 
   formRegistro: FormGroup = new FormGroup({
     vehiculos_id: new FormControl('', [
@@ -40,22 +50,63 @@ export class NuevaReparacionComponent {
     ])
   })
 
+  fechaReparacion() {
+    const fecha = new Date
+    this.fecha = fecha.toLocaleString().substring(0, 20)
+
+  }
+
   async ngOnInit() {
     await this.loadMecanicos()
     await this.loadVehiculos()
+    await this.loadCliente()
+
   }
 
   async onSubmit() {
+    this.fechaReparacion()
     try {
       if (this.formRegistro.valid) {
-        await this.reparacionesServices.register(this.formRegistro.value);
-        console.log(this.formRegistro.value)
-        toast.success('Reparación registrada correctamente');
+        this.nuevaReparacion = await this.reparacionesServices.register(this.formRegistro.value);
+        this.formRegistro.reset()
+        toast.success('Confirmación de ingreso de su vehículo en Taller Macarroni');
+        await this.mailingService.sendMail({
+          nombre: this.cliente.nombre,
+          email: this.cliente.email,
+          mensaje: `Hola ${this.cliente.nombre},
+
+Gracias por confiar en Taller Macarroni.
+
+Te confirmamos que hemos recibido tu vehículo en día y hora ${this.fecha} para la revisión/reparación solicitada. Nuestro equipo ya está trabajando en el diagnóstico y te mantendremos informado sobre el estado de la reparación y cualquier detalle adicional que debamos comentarte.
+
+Datos del ingreso:
+
+    Vehículo: 
+
+    Motivo de la visita: [Descripción breve del problema o servicio solicitado]
+
+    Número de nota de taller: ${this.nuevaReparacion.id}
+
+    Estado actual: ${this.nuevaReparacion.estado}
+
+Saludos,
+Equipo de Taller Macarroni
+[Teléfono del taller] – [Correo del taller]
+  
+  📍 Dirección: C/ Rueda nº 123, Ciudad Motor  
+  📞 Teléfono: 123 456 789  
+  🌐 Web: www.tallermacarroni.com
+  
+  No dudes en escribirnos si necesitas ayuda o tienes alguna pregunta.
+  
+
+  **Taller Macarroni – Donde tu coche está en buenas manos.**`
+        });
+
       } else {
         toast.error('Por favor completa correctamente el formulario');
       }
     } catch (error) {
-      console.log(error);
       toast.error('Hubo un error al registrar la reparación');
     }
   }
@@ -76,14 +127,20 @@ export class NuevaReparacionComponent {
     // biome-ignore lint/suspicious/noExplicitAny: <explanation>
     this.activatedRoute.parent!.params.subscribe(async (params: any) => {
       this.clienteId = params.clienteId
-      this.vehiculos = await this.vehiculosService.getVehiculosByClienteId(this.clienteId)
     });
-
-
+    this.vehiculos = await this.vehiculosService.getVehiculosByClienteId(this.clienteId)
+  }
+  async loadCliente() {
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    this.activatedRoute.parent!.params.subscribe(async (params: any) => {
+      this.clienteId = params.clienteId
+      this.cliente = await this.clientesService.getById(this.clienteId)
+    });
   }
 
-  //checkControl(controlName: string, errorName: string): boolean {
-  //const control = this.formRegistro.get(controlName);
-  //return !!control && control.hasError(errorName) && control.touched;
-  //}
+  checkControl(controlName: string, errorName: string): boolean {
+    const control = this.formRegistro.get(controlName);
+    return !!control && control.hasError(errorName) && control.touched;
+  }
 }
